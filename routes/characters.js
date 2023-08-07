0d0a7981393fabec186bee869860ccba13f4f994
Prakash Shahi Thakuri <prakashthakuri1994@gmail.com>
2023-08-06 21:59:28 -0700

    Update characters.js

--- routes/characters.js
+++ routes/characters.js
@@ -22,7 +22,7 @@ charactersRouter.get( '/', async (req, res) => {
     ); 
   } catch (error) {
     console.error('Failed to fetch data from MongoDB:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send('Internal Server Error', dbClient, collection);
   }
 });
 charactersRouter.get(ROUTES.CHARACTER, async (req, res) => {
